72b63e77a6dee456c31b38484a413825cc2ae1a0
uller91 <girnikis@gmail.com>
2026-02-16 19:51:17 -0800

    stemming for keyword search

--- cli/internal/process_input.py
+++ cli/internal/process_input.py
@@ -1,5 +1,6 @@
 import string
 from internal.process_files import get_stop_words
+from nltk.stem import PorterStemmer
 
 def input_clean(input) -> str:
     frm = ""
@@ -12,7 +13,9 @@ def input_tokenize(input) -> list[str]:
     clean_input = input_clean(input)
     token_input = list(filter(lambda x: x != " ", clean_input.split()))
     no_stop_words_token_input = remove_stop_words(token_input, get_stop_words())
-    return no_stop_words_token_input
+    stemmed_input = reduce_to_stem(no_stop_words_token_input)
+    #print(stemmed_input)
+    return stemmed_input
 
 def match_tokens(tkns1, tkns2) -> bool:
     for tk1 in tkns1:
@@ -23,3 +26,11 @@ def match_tokens(tkns1, tkns2) -> bool:
 
 def remove_stop_words(input, stop_list) -> list[str]:
     return list(filter(lambda x: x not in stop_list, input))
+
+def reduce_to_stem(input) -> list[str]:
+    stemmer = PorterStemmer()
+    stemmed_input = []
+    for token in input:
+        stemmed_word = stemmer.stem(token)
+        stemmed_input.append(stemmed_word)
+    return list(set(stemmed_input))
