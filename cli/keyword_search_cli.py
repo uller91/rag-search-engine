749711b0cf09db8f3c5e24178525f64faa457118
uller91 <girnikis@gmail.com>
2026-02-25 19:37:18 -0800

    TF-IDF

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -15,12 +15,16 @@ def main() -> None:
 
     build_parser = subparsers.add_parser("build", help="Build index for movies")
 
-    tf_parser = subparsers.add_parser("tf", help="Return the term frequency in the film description/title. Usage: tf film_id term")
+    tf_parser = subparsers.add_parser("tf", help="Returns the term frequency in the film description/title. Usage: tf film_id term")
     tf_parser.add_argument("id", type=int, help="Movie ID")
     tf_parser.add_argument("term", type=str, help="Search term")
 
     idf_parser = subparsers.add_parser("idf", help="Prints Inverse Document Frequency for the given term. Usage: idf term")
     idf_parser.add_argument("term", type=str, help="Search term")
+
+    tfidf_parser = subparsers.add_parser("tfidf", help="Returns IF-IDF score. Usage: tf film_id term")
+    tfidf_parser.add_argument("id", type=int, help="Movie ID")
+    tfidf_parser.add_argument("term", type=str, help="Search term")
 
     args = parser.parse_args()
     match args.command:
@@ -62,6 +66,15 @@ def main() -> None:
 
             idf = index.get_idf(args.term)
             print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
+        case "tfidf":
+            try:
+                index.load() #load an index from cache
+            except Exception as e:
+                print(e)
+                return
+
+            tf_idf = index.get_tf(args.id, args.term) * index.get_idf(args.term)
+            print(f"TF-IDF score of '{args.term}' in document '{args.id}': {tf_idf:.2f}")
         case _:
             parser.print_help()
 
